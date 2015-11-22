daddf59e256d73cd7e9b7ab1695636ef7cb4bd1e
Weicheng <wm724@nyu.edu>
2015-11-21 21:16:41 -0500

    CSS

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,15 @@
 Rails.application.routes.draw do
-  get 'home/index'
+  get 'index' => 'home#index'
 
-  get 'home/work'
+  get 'work' => 'home#work'
 
-  get 'home/interest'
+  get 'interest' => 'home#interest'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-   root 'home#index'
+  root 'home#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
