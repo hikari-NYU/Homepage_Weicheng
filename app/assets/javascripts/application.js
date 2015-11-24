eeb37dd8f211a628c77b44b602eea7f0fa549516
Weicheng <wm724@nyu.edu>
2015-11-24 16:52:15 -0500

    CSS

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -38,7 +38,7 @@ $(document).on('click','#home_link',function (event){
 	var i=0;
 	var c = document.getElementById("load_cover");
 	var ctx = c.getContext("2d");
-	ctx.font = "5px Times";
+	ctx.font = "15px Times";
 	var id=setInterval(function() {
 		sub=msg.slice(0,i);
 		ctx.fillText(sub,10,50);
