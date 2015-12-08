a4670af21df933f7a804d57be1634e04b6354821
Weicheng <wm724@nyu.edu>
2015-12-08 09:50:35 -0500

    CSS

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,4 +7,4 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 SelfIntro.create(name: 'Weicheng Ma', university: 'City University of New York', email: 'Email: weicheng.ma1991@gmail.com', phone: 'Phone: +1(347) 933-3468', intro:'', lang: 'English')
 SelfIntro.create(name: '马伟诚', university: '纽约城市大学', email: '电子邮件: weicheng.ma1991@gmail.com', phone: '电话: +1(347) 933-3468', intro:'', lang: 'Chinese')
-SelfIntro.create(name: '馬偉誠', university: 'City University of New York', email: 'メール: weicheng.ma1991@gmail.com', phone: '携帯 +1(347) 933-3468', intro:'', lang: 'Japanese')
+SelfIntro.create(name: '馬偉誠', university: 'City University of New York', email: 'メール: weicheng.ma1991@gmail.com', phone: '携帯: +1(347) 933-3468', intro:'', lang: 'Japanese')
