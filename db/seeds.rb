de1791063512c2e7d2173645877033e0d2e6fa48
Weicheng <wm724@nyu.edu>
2015-12-08 12:47:25 -0500

    CSS

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,6 +5,7 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+LinkLang.delete_all
 LinkLang.create(link:'home', lang:'English', types:'home')
 LinkLang.create(link:'首页', lang:'Chinese', types:'home')
 LinkLang.create(link:'インデックス', lang:'Japanese', types:'home')
@@ -14,6 +15,7 @@ LinkLang.create(link:'作品', lang:'Japanese', types:'work')
 LinkLang.create(link:'interest', lang:'English', types:'interest')
 LinkLang.create(link:'兴趣', lang:'Chinese', types:'interest')
 LinkLang.create(link:'趣味', lang:'Japanese', types:'interest')
+SelfIntro.delete_all
 SelfIntro.create(name: 'Weicheng Ma', university: 'City University of New York', email: 'Email: weicheng.ma1991@gmail.com      ', phone: 'Phone: +1(347) 933-3468', intro:'', lang: 'English')
 SelfIntro.create(name: '马伟诚', university: '纽约城市大学', email: '电子邮件: weicheng.ma1991@gmail.com', phone: '电话: +1(347) 933-3468', intro:'', lang: 'Chinese')
 SelfIntro.create(name: '馬偉誠', university: 'ニューヨーク市立大学', email: 'メール: weicheng.ma1991@gmail.com', phone: '携帯: +1(347) 933-3468', intro:'', lang: 'Japanese')
