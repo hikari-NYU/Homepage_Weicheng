d23009bc021ab00490d2367ba771c30f6350e1aa
Weicheng <wm724@nyu.edu>
2016-06-15 09:14:51 -0400

    CSS

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -62,7 +62,7 @@ Rails.application.configure do
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
-  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
@@ -81,10 +81,10 @@ Rails.application.configure do
   config.assets.compress = false
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
-    :address              => 'smtp.nyu.edu',
+    :address              => 'smtp.gmail.com',
     :port                 => 587,
-    :user_name            => 'wm724@nyu.edu',
-    :password             => '0628Melody',
+    :user_name            => 'weicheng.ma1991@gmail.com',
+    :password             => 'SM0713tt',
     :authentication       => 'plain',
     :enable_starttls_auto => true
   }
